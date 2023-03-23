06c1ba8cd4b3a19c100a663713c6b331fe668676
yeeecheng <yichengliao1003@gmail.com>
2023-03-23 17:34:39 +0800

    update model and anaylize

--- anaylize_Data/anaylize_QTYofLOB_change.py
+++ anaylize_Data/anaylize_QTYofLOB_change.py
@@ -27,6 +27,7 @@ def div(x, y):
 # 檔案位置處理
 filepath = "./stock_dataset"
 day = 59
+tick = 474457 
 symbols_list = os.listdir(filepath)
 
 for symbol  in symbols_list:
@@ -40,7 +41,7 @@ for symbol  in symbols_list:
     # 建立計算數量的list 
     # 買1的變化(上漲、平盤、下跌) 配 賣1的變化(上漲、平盤、下跌)，對應下筆成交價上漲、平盤、下跌 , 3 * 3 * 3 = 27個
     prob = list()
-    for _ in range(27):
+    for _ in range(99):
         # [上漲、平盤、下跌]
         prob.append([0,0,0])
 
@@ -66,6 +67,30 @@ for symbol  in symbols_list:
                 # 如果是 目前成交價 = 中間價
                 elif data.iloc[i]["askPri1"] > data.iloc[i]["matchPri"]  and data.iloc[i]["matchPri"] > data.iloc[i]["bidPri1"]:
                     padding = 18
+                # 如果是 目前成交價 = 買2
+                elif data.iloc[i]["bidPri2"] == data.iloc[i]["matchPri"]:
+                    padding = 27
+                # 如果是 目前成交價 = 賣2
+                elif data.iloc[i]["askPri2"] == data.iloc[i]["matchPri"]:
+                    padding = 36
+                # 如果是 目前成交價 = 買3
+                elif data.iloc[i]["bidPri3"] == data.iloc[i]["matchPri"]:
+                    padding = 45
+                # 如果是 目前成交價 = 賣3
+                elif data.iloc[i]["askPri3"] == data.iloc[i]["matchPri"]:
+                    padding = 54
+                # 如果是 目前成交價 = 買4
+                elif data.iloc[i]["bidPri4"] == data.iloc[i]["matchPri"]:
+                    padding = 63
+                # 如果是 目前成交價 = 賣4
+                elif data.iloc[i]["askPri4"] == data.iloc[i]["matchPri"]:
+                    padding = 72
+                # 如果是 目前成交價 = 買5
+                elif data.iloc[i]["bidPri5"] == data.iloc[i]["matchPri"]:
+                    padding = 81
+                # 如果是 目前成交價 = 賣5
+                elif data.iloc[i]["askPri5"] == data.iloc[i]["matchPri"]:
+                    padding = 90
                 # 其他的不考慮
                 else :
                     continue
@@ -106,9 +131,40 @@ for symbol  in symbols_list:
                 elif data.iloc[i+1]["bidPri1"] < data.iloc[i]["bidPri1"] and  data.iloc[i+1]["askPri1"] < data.iloc[i]["askPri1"]:
                     
                     Count(data.iloc[i+1]["matchPri"],data.iloc[i]["matchPri"],8+padding,prob)
-                    
+            tick-=1
+            if not tick : break
         # 計算天數
-        day -=1
-        if not day : break
+        
+        #day -=1
+        #if not day : break
+        if not tick : break
     break
+
+
+
+# 輸出
+print(f"tick 相差 {gap}")
+item = ["bid1","ask1","中間價","bid2","ask2","bid3","ask3","bid4","ask4","bid5","ask5"]
+print(len(prob))
+for i in range(len(item)):
+    total = sum([sum(prob[j+9*i])  for j in range(0,9)])
+    print(f"\n目前這筆的 成交價是 目前這筆的 {item[i]} 時")
+    print('bid1 下一筆變成 up   ,ask1 下一筆變成 up   : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[0+9*i]),total),div(prob[0+9*i][0],sum(prob[0+9*i])),div(prob[0+9*i][1],sum(prob[0+9*i])),div(prob[0+9*i][2],sum(prob[0+9*i]))))
+    print('bid1 下一筆變成 up   ,ask1 下一筆變成 平   : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[1+9*i]),total),div(prob[1+9*i][0],sum(prob[1+9*i])),div(prob[1+9*i][1],sum(prob[1+9*i])),div(prob[1+9*i][2],sum(prob[1+9*i]))))
+    print('bid1 下一筆變成 up   ,ask1 下一筆變成 down : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[2+9*i]),total),div(prob[2+9*i][0],sum(prob[2+9*i])),div(prob[2+9*i][1],sum(prob[2+9*i])),div(prob[2+9*i][2],sum(prob[2+9*i]))))
+    print('bid1 下一筆變成 平   ,ask1 下一筆變成 up   : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[3+9*i]),total),div(prob[3+9*i][0],sum(prob[3+9*i])),div(prob[3+9*i][1],sum(prob[3+9*i])),div(prob[3+9*i][2],sum(prob[3+9*i]))))
+    print('bid1 下一筆變成 平   ,ask1 下一筆變成 平   : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[4+9*i]),total),div(prob[4+9*i][0],sum(prob[4+9*i])),div(prob[4+9*i][1],sum(prob[4+9*i])),div(prob[4+9*i][2],sum(prob[4+9*i]))))
+    print('bid1 下一筆變成 平   ,ask1 下一筆變成 down : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[5+9*i]),total),div(prob[5+9*i][0],sum(prob[5+9*i])),div(prob[5+9*i][1],sum(prob[5+9*i])),div(prob[5+9*i][2],sum(prob[5+9*i]))))
+    print('bid1 下一筆變成 down ,ask1 下一筆變成 up   : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[6+9*i]),total),div(prob[6+9*i][0],sum(prob[6+9*i])),div(prob[6+9*i][1],sum(prob[6+9*i])),div(prob[6+9*i][2],sum(prob[6+9*i]))))
+    print('bid1 下一筆變成 down ,ask1 下一筆變成 平   : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[7+9*i]),total),div(prob[7+9*i][0],sum(prob[7+9*i])),div(prob[7+9*i][1],sum(prob[7+9*i])),div(prob[7+9*i][2],sum(prob[7+9*i]))))
+    print('bid1 下一筆變成 down ,ask1 下一筆變成 down : {:.4f} ,下一筆成交價上漲: {:.4f} , 平盤: {:.4f} ,下跌: {:.4f}'.format(
+        div(sum(prob[8+9*i]),total),div(prob[8+9*i][0],sum(prob[8+9*i])),div(prob[8+9*i][1],sum(prob[8+9*i])),div(prob[8+9*i][2],sum(prob[8+9*i]))))
 
